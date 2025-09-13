c9163a42ff7df50f6918d3ba3f73c5a580bb9dd4
UmarRabi <tipson664@gmail.com>
2025-09-14 00:57:33 +0100

    added adjustment on navigations

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/', // Change if needed
-  // baseURL: 'https://drink-backend-kfef.onrender.com/api/v1/', // Change if needed
+  // baseURL: 'http://localhost:3000/api/v1/', // Change if needed
+  baseURL: 'https://drink-backend-kfef.onrender.com/api/v1/', // Change if needed
   headers: {
     'Content-Type': 'application/json',
   },
